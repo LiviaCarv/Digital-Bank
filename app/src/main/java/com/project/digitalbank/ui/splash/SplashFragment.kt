e45b9e5c0fe2b0af60705a69af8845d4054d0175
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-04-25 18:42:10 -0400

    configured view binding; SplashFragment created

---
+++ app/src/main/java/com/project/digitalbank/ui/splash/SplashFragment.kt
@@ -0,0 +1,30 @@
+package com.project.digitalbank.ui.splash
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.project.digitalbank.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
