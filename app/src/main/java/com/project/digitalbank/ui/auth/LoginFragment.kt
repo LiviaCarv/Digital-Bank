7da2075c45e48b53e6f420687be4ea5678c7c5cd
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-04-25 20:25:01 -0400

    created LoginFragment

---
+++ app/src/main/java/com/project/digitalbank/ui/auth/LoginFragment.kt
@@ -0,0 +1,30 @@
+package com.project.digitalbank.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.project.digitalbank.R
+import com.project.digitalbank.databinding.FragmentLoginBinding
+import com.project.digitalbank.databinding.FragmentSplashBinding
+
+class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
