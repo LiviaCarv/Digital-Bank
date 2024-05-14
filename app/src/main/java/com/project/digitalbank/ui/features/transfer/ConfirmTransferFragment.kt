131e6716ac494849351065248d8ef0d5a355d39f
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-05-13 20:46:39 -0400

    displaying user data on the transfer confirmation screen

--- app/src/main/java/com/project/digitalbank/ui/features/transfer/ConfirmTransferFragment.kt
+++ app/src/main/java/com/project/digitalbank/ui/features/transfer/ConfirmTransferFragment.kt
@@ -4,29 +4,67 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.project.digitalbank.R
+import com.project.digitalbank.data.model.User
 import com.project.digitalbank.databinding.FragmentConfirmTransferBinding
+import com.project.digitalbank.databinding.FragmentDepositReceiptBinding
+import com.project.digitalbank.util.GetMask
+import com.project.digitalbank.util.initToolBar
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import java.lang.Exception
 
 class ConfirmTransferFragment : Fragment() {
 
 
     private var _binding: FragmentConfirmTransferBinding? = null
     private val binding get() = _binding!!
+    private val args: ConfirmTransferFragmentArgs by navArgs()
+    private val tagPicasso = "tagPicasso"
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentConfirmTransferBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initToolBar(binding.toolbar, homeAsUpEnabled = true)
+        configData(args.user)
 
+    }
+
+    private fun configData(user: User) {
+        binding.imgDestIcon.isVisible = true
+        binding.txtUserTransfer.text = user.name
+        binding.txtShowTransferValue.text = getString(R.string.text_account_balance_format, GetMask.getFormattedValue(args.amount))
+        Picasso
+            .get()
+            .load(user.imageProfile)
+            .tag(tagPicasso)
+            .fit().centerCrop()
+            .into(binding.imgDestIcon, object : Callback {
+                override fun onSuccess() {
+                    binding.imgDestIcon.isVisible = true
+                    binding.imgProgressBar.isVisible = false
+                }
+                override fun onError(e: Exception?) {
+                    TODO("Not yet implemented")
+                }
+            })
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        Picasso.get().cancelTag(tagPicasso)
+
         _binding = null
     }
 
